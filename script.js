3b3ad4a245ba5756092ce57986d49d9152fa9659
River Fisher <river.bass.fisher@gmail.com>
2021-10-02 12:19:08 -0500

    adds comment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Returns string 'rock', 'paper', or 'scissors' based on a random number.
 function computerPlay(){
     let numBetween1And3 = Math.floor( Math.random() * 3 + 1 );
     if( numBetween1And3 === 1 ){
@@ -7,5 +7,15 @@ function computerPlay(){
         return 'paper';
     }else{
         return 'scissors';
+    }
+}
+
+function playRound( playerSelection, computerSelection ){
+    if( playerSelection === computerSelection ){
+        return "Draw!";
+    }else if( ( playerSelection === 'rock' && computerSelection === 'paper' ) || ( playerSelection === 'paper' && computerSelection === 'scissors' ) || ( playerSelection === 'scissors' && computerSelection === 'rock' ) ){
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }else{
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     }
 }
